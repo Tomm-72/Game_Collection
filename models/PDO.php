e0026cb21bfa1f6b6a252731f78e118eaa7294de
LeMoutMout <pitchoun.theo@gmail.com>
2023-12-15 11:50:42 +0100

    env et pdo to acces db

---
+++ models/PDO.php
@@ -0,0 +1,9 @@
+<?php 
+
+function dbConnect()
+{
+    //Connexion à la bdd
+    $bdd = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] , $_ENV['DB_USER'] , $_ENV['DB_PASS'] , array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+
+    return $bdd;
+}
