ab3efbdff2154307d6b5a332e5b8d116b7c1ba09
LeMoutMout <pitchoun.theo@gmail.com>
2023-12-15 12:12:28 +0100

    model user finished

--- models/PDO.php
+++ models/PDO.php
@@ -1,6 +1,6 @@
 <?php 
 
-function dbConnect()
+function getDB()
 {
     //Connexion à la bdd
     $bdd = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] , $_ENV['DB_USER'] , $_ENV['DB_PASS'] , array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
---
+++ models/user_model.php
@@ -0,0 +1,38 @@
+<?php
+function createUser($nom,$prenom,$mdp,$mail) {
+    $db = getDB();
+    
+    $db_query_create = $db->prepare('insert into Utilisateur(nom_user,pren_user,mdp_user,mail_user) values(:nom,:pren,:mdp,:mail)');
+    
+    $db_query_create->execute([
+        'nom' => $nom,
+        'pren' => $prenom,
+        'mdp' => $mdp,
+        'mail' => $mail
+    ]);
+}
+
+function updateUser($id_user, $nom, $prenom, $mdp, $mail ){
+    $db = getDB();
+
+    $db_query_update =$db->prepare('update Utilisateur SET nom_user = :nom, pren_user = :prenom, mdp_user = :mdp, mail_user = :mail WHERE id_user = :id');
+
+    $db_query_update->execute([
+        'nom' => $nom,
+        'pren' => $prenom,
+        'mdp' => $mdp,
+        'mail' => $mail,
+        'id' => $id_user
+    ]);
+}
+
+function deleteUser($id_user){
+    $db = getDB();
+
+    $db_query_delete = $db->prepare('delete FROM Utilisateur WHERE id_user = :id');
+
+    $db_query_delete->execute([
+        'id' => $id_user
+    ]);
+}
+?>
