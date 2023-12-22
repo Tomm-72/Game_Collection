1e973c5da2cabd89ea4825aedd9207d6e7dc945f
LeMoutMout <pitchoun.theo@gmail.com>
2023-12-22 15:56:05 +0100

    debut connexion

--- controllers/connexion_page.php
+++ controllers/connexion_page.php
@@ -3,4 +3,9 @@
 require "../models/PDO.php";
 require "../models/user_model.php";
 
-
+if(isset($_POST['email']) && isset($_POST['password'])){
+    $mdp = getMDPUser($_POST['email']);
+    if($mdp == $_POST['password']){
+        
+    }
+}
