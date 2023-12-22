b66f94f480c2fe3fa2ce30b4ded748d03d33e338
LeMoutMout <pitchoun.theo@gmail.com>
2023-12-22 16:23:28 +0100

    debut connexion page

--- controllers/connexion_page.php
+++ controllers/connexion_page.php
@@ -3,9 +3,14 @@
 require "../models/PDO.php";
 require "../models/user_model.php";
 
+session_start();
+
 if(isset($_POST['email']) && isset($_POST['password'])){
     $mdp = getMDPUser($_POST['email']);
+    $mdp = password_hash($mdp, PASSWORD_BCRYPT);
     if($mdp == $_POST['password']){
-        
+        $_SESSION['id']=getIdUser($mail);
+    }else{
+        echo "l'email et le mot de passe ne correspondent pas";
     }
 }
