454c1bc9dd1a7d458cbc770725fa337dbe379464
Tomm-72 <114407431+Tomm-72@users.noreply.github.com>
2024-01-16 19:28:05 +0100

    Edit the footer

--- controllers/home_page.php
+++ controllers/home_page.php
@@ -3,4 +3,5 @@
 require_once ('models/user_model.php');
 require_once ('lib/component/header.php');
 require_once ('views/home_page.php');
+require_once ('lib/component/footer.php');
 
---
+++ lib/component/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="src/styles/footer.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="footer">
+    <p>Game Collection - 2023 - Tous droits réservés</p>
+</div>
+</body>
+</html>
