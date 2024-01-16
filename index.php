e2449f14c02fef3b8f3f596c6eb6fd98da225390
Tomm-72 <114407431+Tomm-72@users.noreply.github.com>
2024-01-16 17:38:06 +0100

    fixe bug when first loading TODO : fix css bugs ; finish the css

--- index.php
+++ index.php
@@ -1,23 +1,27 @@
 <?php
-    require_once 'vendor/autoload.php';
-    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-    $dotenv->load();
-    $db = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] , $_ENV['DB_USER'] , $_ENV['DB_PASS'] , array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+require_once 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$db = new PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'], $_ENV['DB_USER'], $_ENV['DB_PASS'], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 session_start();
-    if (isset($_GET['page'])) {
-        switch ($_GET['page']) {
-            case 'accueil':
-                require_once ('controllers/home_page.php');
-                break;
-            case 'connexion':
-                require_once ('controllers/connexion_page.php');
-                break;
-            case 'inscription':
-                require_once ('controllers/inscription_page.php');
-                break;
-            default:
-                header('Location: connexion');
-        }
+if (isset($_GET['page'])) {
+    switch ($_GET['page']) {
+        case 'accueil':
+            require_once('controllers/home_page.php');
+            break;
+        case 'connexion':
+            require_once('controllers/connexion_page.php');
+            break;
+        case 'inscription':
+            require_once('controllers/inscription_page.php');
+            break;
+        default:
+            header('Location: connexion');
+            break;
     }
+} else {
+    header('Location: connexion');
+    exit;
+}
 ?>
