05c7948a61b58ffccf31a28b6d6722db426f40f9
Grizef <50044553+Grizef@users.noreply.github.com>
2024-01-16 17:27:27 +0100

    homepage

--- views/home_page.php
+++ views/home_page.php
@@ -12,11 +12,14 @@
         <img id="imgHome" src="src\styles\img\imgHome.svg" alt="IMAGE">
         <h2>Mes jeux</h2>
         <div id="jeu">
-            <img id="jeuImg"src="src\styles\img\testImg.jpg" alt="IMG">
+            <img id="jeuImg" src="https://blog.fr.playstation.com/tachyon/sites/10/2022/03/b260da887312ad9afff5c4c1aba8f317fa6ce6e0.jpg?resize=1088%2C612&crop_strategy=smart&zoom=1.5" alt="IMG">
             <div id="txt">
                 <h3>NomJeu</h3>
-                <p>Heure</p>
-                <p>Plateforme</p>
+                <div>
+                    <p class=info>Heure</p>
+                    <p class=info>Plateforme</p> 
+                </div>
+                
             </div>
             
         </div>
